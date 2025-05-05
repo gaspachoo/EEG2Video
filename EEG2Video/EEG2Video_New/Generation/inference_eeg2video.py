8a8031ceb7f20b4c86d72c142063ea0998692b98
Gaspachoo <gaspard.lefort@outlook.fr>
2025-05-05 12:07:03 +0530

    edit files

--- EEG2Video/EEG2Video_New/Generation/inference_eeg2video.py
+++ EEG2Video/EEG2Video_New/Generation/inference_eeg2video.py
@@ -23,7 +23,8 @@ from diffusers.schedulers import (
     DDPMScheduler,
     DDIMScheduler,
 )
-torch.cuda.set_device(2)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Running on device:", device)
 def seed_everything(seed=0, cudnn_deterministic=True):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -45,39 +46,42 @@ negative = eeg.mean(dim=0)
 pretrained_model_path = "Zhoutianyi/huggingface/stable-diffusion-v1-4"
 my_model_path = "outputs/40_classes_200_epoch"
 
-unet = UNet3DConditionModel.from_pretrained(my_model_path, subfolder='unet', torch_dtype=torch.float16).to('cuda')
-pipe = TuneAVideoPipeline.from_pretrained(pretrained_model_path ,unet=unet, torch_dtype=torch.float16).to("cuda")
+unet = UNet3DConditionModel.from_pretrained(my_model_path, subfolder='unet', torch_dtype=torch.float16).to(device)
+pipe = TuneAVideoPipeline.from_pretrained(pretrained_model_path ,unet=unet, torch_dtype=torch.float16).to(device)
 pipe.enable_xformers_memory_efficient_attention()
 pipe.enable_vae_slicing()
 
 
-
-latents = np.load('Seq2Seq/latent_out_block7_40_classes.npy')
-latents = torch.from_numpy(latents).half()
-latents = rearrange(latents, 'a b c d e -> a c b d e')
-latents = latents.to('cuda')
-
-latents_add_noise = torch.load('DANA/40_classes_latent_add_noise.pt')
-latents_add_noise = latents_add_noise.half()
-latents_add_noise = rearrange(latents_add_noise, 'a b c d e -> a c b d e')
-latents_add_noise = latents_add_noise.to('cuda')
 # Ablation, inference w/o Seq2Seq and w/o DANA
 woSeq2Seq = False
-woDANA = False
+woDANA = True
+
+if woSeq2Seq:
+    latents = np.load('Seq2Seq/latent_out_block7_40_classes.npy')
+    latents = torch.from_numpy(latents).half()
+    latents = rearrange(latents, 'a b c d e -> a c b d e')
+    latents = latents.to(device)
+
+if woDANA:
+    latents_add_noise = torch.load('DANA/40_classes_latent_add_noise.pt')
+    latents_add_noise = latents_add_noise.half()
+    latents_add_noise = rearrange(latents_add_noise, 'a b c d e -> a c b d e')
+    latents_add_noise = latents_add_noise.to(device)
+
 for i in range(len(eeg)):
     if woSeq2Seq:
         video = pipe(model, eeg[i:i+1,...],negative_eeg=negative, latents=None, video_length=6, height=288, width=512, num_inference_steps=100, guidance_scale=12.5).videos
-        savename = f'40_Classes_woSeq2Seq/EEG2Video/'
+        savename = f'../../../40_Classes_woSeq2Seq/'
         import os
         os.makedirs(savename, exist_ok=True)
     elif woDANA:
         video = pipe(model, eeg[i:i+1,...],negative_eeg=negative, latents=latents[i:i+1,...], video_length=6, height=288, width=512, num_inference_steps=100, guidance_scale=12.5).videos
-        savename = f'40_Classes_woDANA/EEG2Video/'
+        savename = f'../../../40_Classes_woDANA/'
         import os
         os.makedirs(savename, exist_ok=True)
     else:
         video = pipe(model, eeg[i:i+1,...],negative_eeg=negative, latents=latents_add_noise[i:i+1,...], video_length=6, height=288, width=512, num_inference_steps=100, guidance_scale=12.5).videos
-        savename = f'40_Classes_Fullmodel/EEG2Video/'
+        savename = f'../../../40_Classes_Fullmodel/'
         import os
         os.makedirs(savename, exist_ok=True)
     save_videos_grid(video, f"./{savename}/{i}.gif")
