ef013d5b76ca86fc39d06ecca9cf9efd16719ed7
Gaspachoo <gaspard.lefort@outlook.fr>
2025-05-05 19:00:31 +0530

    edit files

--- Gaspard_model/train_seq2seq.py
+++ Gaspard_model/train_seq2seq.py
@@ -7,6 +7,7 @@ import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 import wandb
+import torch.nn.functional as F
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=500):
@@ -32,8 +33,8 @@ class Seq2SeqTransformer(nn.Module):
         super().__init__()
 
         self.input_proj = nn.Linear(512, d_model)   # EEG embedding dim → d_model
-        self.output_proj = nn.Linear(256, d_model)  # Latent dim → d_model
-        self.out_linear = nn.Linear(d_model, 256)   # Predicted output dim
+        self.output_proj = nn.Linear(9216, d_model)  # Latent dim → d_model
+        self.out_linear = nn.Linear(d_model, 9216)   # Predicted output dim
 
         self.pos_encoder = PositionalEncoding(d_model, dropout)
         self.pos_decoder = PositionalEncoding(d_model, dropout)
@@ -45,7 +46,7 @@ class Seq2SeqTransformer(nn.Module):
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_decoder_layers)
 
     def forward(self, src, tgt):
-        # src: (B, 7, 512), tgt: (B, 6, 256)
+        # src: (B, 7, 512), tgt: (B, 6, 9216)
         src = self.input_proj(src)          # (B, 7, d_model)
         tgt = self.output_proj(tgt)         # (B, 6, d_model)
 
@@ -64,74 +65,84 @@ def parse_args():
     import argparse
     parser = argparse.ArgumentParser()
     root = os.environ.get("HOME", os.environ.get("USERPROFILE")) + "/EEG2Video" #"/Documents/School/Centrale Med/2A/SSE/EEG2Video"
-    parser.add_argument('--eeg_dir', type=str, default=f"{root}/data/EEG_embeddings/", help='Path to EEG z_hat files')
+    parser.add_argument('--sub_emb', type=str, default=f"{root}/data/EEG_embeddings/sub3.npy", help='Path to EEG z_hat file')
     parser.add_argument('--video_dir', type=str, default=f'{root}/data/Video_latents', help='Path to block-wise video latent files')
-    parser.add_argument('--save_path', type=str, default=f'./checkpoints/seq2seq/seq2seq_transformer.pth')
+    parser.add_argument('--save_path', type=str, default=f'{root}/Gaspard_model/checkpoints/seq2seq/')
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--use_wandb', action='store_true', help='Log metrics to wandb')
     return parser.parse_args()
 
-def prepare_data(eeg_dir, video_dir):
-    all_eeg = []
-    for subj_file in sorted(os.listdir(eeg_dir)):
-        if not subj_file.endswith('.npy'):
-            continue
-        eeg_path = os.path.join(eeg_dir, subj_file)
-        z_hat = np.load(eeg_path)  # (7, 40, 5, 2, 512)
-        print(f"Loaded EEG: {subj_file}, shape={z_hat.shape}")
-        z_hat = z_hat.reshape(7, 40, 5, 2, 512)
-        z_hat = z_hat[..., 0, :]  # Keep only first 1s EEG → shape: (7, 40, 5, 512)
-        z_hat = z_hat.reshape(-1, 7, 512)  # (200, 7, 512)
-        all_eeg.append(z_hat)
+def prepare_data(sub_emb, video_dir, block_id):
+    z_hat = np.load(sub_emb)  # (7, 40, 5, 2, 512)
+    #print(f"Loaded EEG: {sub_emb}, shape={z_hat.shape}")
+    z_hat = z_hat.reshape(7, 40, 5, 2, 512)
+    z_hat = z_hat[..., 0, :].transpose(1,2,0,3)  # Keep only first 1s EEG → shape: (7, 40, 5, 512) -> transpose to (40, 5, 7, 512)
+    z_hat = z_hat.reshape(-1, 7, 512)  # (200, 7, 512)
+    
 
-    vid_path = os.path.join(video_dir, 'block0_latents.npy')  # Match same block as EEG
+    vid_path = os.path.join(video_dir, f'block{block_id}_latents.npy')  # Match same block as EEG
     z0 = np.load(vid_path)  # (200, 6, 4, 36, 64)
-    print(f"Loaded Latents: block0_latents.npy, shape={z0.shape}")
-    z0 = z0.reshape(z0.shape[0], 6, -1)[:, :, :256]  # (200, 6, 256)
+    #print(f"Loaded Latents: block{block_id}_latents.npy, shape={z0.shape}")
+    z0 = z0.reshape(z0.shape[0], 6, -1) # (200, 6, 9216)
 
-    eeg_tensor = torch.tensor(np.concatenate(all_eeg), dtype=torch.float32)
+    eeg_tensor = torch.tensor(z_hat, dtype=torch.float32)
     vid_tensor = torch.tensor(z0, dtype=torch.float32)
 
-    print(f"Final EEG tensor shape: {eeg_tensor.shape}")
-    print(f"Final Video tensor shape: {vid_tensor.shape}")
+    #print(f"Final EEG tensor shape: {eeg_tensor.shape}")
+    #print(f"Final Video tensor shape: {vid_tensor.shape}")
 
     return TensorDataset(eeg_tensor, vid_tensor)
 
-def train_seq2seq():
-    args = parse_args()
+
+def cosine_similarity(pred, target):
+    pred = F.normalize(pred, dim=-1)
+    target = F.normalize(target, dim=-1)
+    return (pred * target).sum(dim=-1).mean().item()
+
+
+def train_seq2seq(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
+    block_id = 0
+    
+    for block_id in tqdm(range(7),desc="Loading blocks"):
+        dataset = prepare_data(args.sub_emb, args.video_dir, block_id=block_id)
+        dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+        
+        model = Seq2SeqTransformer().to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        criterion = nn.MSELoss()
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs * len(dataloader))
 
-    dataset = prepare_data(args.eeg_dir, args.video_dir)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
-
-    model = Seq2SeqTransformer().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    criterion = nn.MSELoss()
-
-    if args.use_wandb:
-        wandb.init(project="eeg2video-Seq2eq", name="Seq2Seq", config=vars(args))
-
-    for epoch in range(1, args.epochs + 1):
-        model.train()
-        running_loss = 0
-        for src, tgt in tqdm(dataloader, desc=f"Epoch {epoch}"):
-            src, tgt = src.to(device), tgt.to(device)
-            optimizer.zero_grad()
-            out = model(src, tgt)
-            loss = criterion(out, tgt)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-
-        avg_loss = running_loss / len(dataloader)
-        print(f"Epoch {epoch} - Loss: {avg_loss:.4f}")
         if args.use_wandb:
-            wandb.log({"epoch": epoch, "train_loss": avg_loss})
+            wandb.init(project="eeg2video-Seq2Seq", name=f"Seq2Seq_{block_id}", config=vars(args))
+            wandb.watch(model, log="all")
 
-    torch.save(model.state_dict(), args.save_path)
-    print(f"Model saved to {args.save_path}")
+        for epoch in tqdm(range(1, args.epochs + 1),desc=f"Epochs for block {block_id}"):
+            model.train()
+            running_loss = 0
+            for src, tgt in dataloader:
+                src, tgt = src.to(device), tgt.to(device)
+                optimizer.zero_grad()
+                out = model(src, tgt)
+                loss = criterion(out, tgt)
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+                cos_sim = cosine_similarity(out, tgt)
+                running_loss += loss.item()
+
+            avg_loss = running_loss / len(dataloader)
+            if args.use_wandb:
+                wandb.log({"lr": optimizer.param_groups[0]['lr']})
+                wandb.log({"epoch": epoch, "train_loss": avg_loss, "cosine_similarity": cos_sim})
+            
+        wandb.finish()
+        
+        torch.save(model.state_dict(), os.path.join(args.save_path,f"seq2seq_block{block_id}.pth"))
+        print(f"Model saved to {args.save_path}seq2seq_block{block_id}.pth")
 
 if __name__ == '__main__':
-    train_seq2seq()
+    train_seq2seq(parse_args())
