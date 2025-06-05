dedd969287a333211903c86476fe010a8be6c831
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-05 12:05:01 +0530

    edit to match updated names

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,7 @@ from Gaspard.FullPipeline.segment_raw_signals_single import extract_2s_segment
 from EEG_preprocessing.segment_sliding_window import seg_sliding_window
 from EEG_preprocessing.extract_DE_PSD_features_1per500ms import extract_de_psd_sw
 from Gaspard.GLMNet.inference_glmnet import inf_glmnet, load_glmnet_from_checkpoint
-from Gaspard.Seq2Seq.inference_seq2seq_v2 import inf_seq2seq, load_s2s_model
+from Gaspard.Seq2Seq.inference_seq2seq_v2 import inf_seq2seq, load_s2s_from_checkpoint
 
 if __name__ == "__main__":
     import argparse
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     eeg_embeddings = inf_glmnet(model_glmnet, seven_sw, features_seven_sw, device='cuda')[None, None, None, ...]
     print("EEG embeddings shape:", eeg_embeddings.shape)
     
-    model_s2s = load_s2s_model(args.s2s_path, device='cuda')
+    model_s2s = load_s2s_from_checkpoint(args.s2s_path, device='cuda')
     vid_latents = inf_seq2seq(model_s2s, eeg_embeddings, device='cuda')
     print("Video latents shape:", vid_latents.shape)
     
