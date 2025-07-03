6152103debf04339660320e94c2fe251ca10a709
Gaspard <113727365+gaspachoo@users.noreply.github.com>
2025-07-03 13:27:39 +0530

    translate comments to english

--- EEG_preprocessing/segment_sliding_window.py
+++ EEG_preprocessing/segment_sliding_window.py
@@ -14,17 +14,17 @@ def seg_sliding_window(data, win_s, step_s, fs=200):
     fs : int
         Sampling frequency in Hz (default 200 Hz)"""
     
-    win_t = int(fs * win_s)   # points temporels par fenêtre (100)
-    step_t = int(fs * step_s) # pas entre fenêtres (50)
-    # Sliding window le long de l'axe temporel (-1)
+    win_t = int(fs * win_s)   # number of time points per window (100)
+    step_t = int(fs * step_s) # step between windows (50)
+    # Sliding window along the time axis (-1)
     windows = sliding_window_view(data, window_shape=win_t, axis=-1)
     # windows.shape -> (7, 40, 5, 62, 301, 100)
 
-    # Sous-échantillonner selon le pas STEP_T pour obtenir 7 fenêtres
+    # Subsample with step STEP_T to obtain 7 windows
     windows = windows[..., ::step_t, :]
     # windows.shape -> (7, 40, 5, 62, 7, 100)
 
-    # Réorganiser pour avoir (7, 40, 5, 7, 62, 100)
+    # Rearrange to get (7, 40, 5, 7, 62, 100)
     windows = windows.transpose(0, 1, 2, 4, 3, 5)
 
     return windows
@@ -36,9 +36,9 @@ if __name__ == "__main__":
     INPUT_DIR = './data/Preprocessing/Segmented_Rawf_200Hz_2s'
 
     # Segmenting settings
-    FS = 200                  # fréquence d'échantillonnage (Hz)
-    WIN_S = 0.5               # fenêtre (secondes)
-    STEP_S = 0.25             # recouvrement (secondes)
+    FS = 200                  # sampling frequency (Hz)
+    WIN_S = 0.5               # window (seconds)
+    STEP_S = 0.25             # overlap (seconds)
 
     # Output directory depends on WIN_S
     OUTPUT_DIR = f'./data/Preprocessing/Segmented_{int(1000*WIN_S)}ms_sw'
